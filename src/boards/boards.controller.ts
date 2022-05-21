ef4ce53d39eee451f43e2f6674dd8ce0d6c1fb52
shch989 <tankpop11@gmail.com>
2022-05-21 21:22:06 +0900

    CRUD서비스  구현

--- src/boards/boards.controller.ts
+++ src/boards/boards.controller.ts
@@ -1,5 +1,14 @@
-import { Controller, Get } from '@nestjs/common';
-import { Board } from './board.model';
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  Param,
+  Patch,
+  Post,
+} from '@nestjs/common';
+import { CreateBoardDto } from 'src/dto/create-board.dto';
+import { Board, BoardStatus } from './board.model';
 import { BoardsService } from './boards.service';
 
 @Controller('boards')
@@ -7,7 +16,31 @@ export class BoardsController {
   constructor(private boardsService: BoardsService) {}
   // CRUD 에서의 R
   @Get('/')
-  getAllBoards(): Board[] {
+  getAllBoard(): Board[] {
     return this.boardsService.getAllBoards();
+  }
+  // C 기능 controller
+  @Post()
+  createBoard(@Body() createBoardDto: CreateBoardDto): Board {
+    return this.boardsService.createBoard(createBoardDto);
+  }
+  // 특정 게시물 R controller
+  @Get('/:id')
+  getBoardById(@Param('id') id: string): Board {
+    return this.boardsService.getBoardById(id);
+  }
+  // 특정 게시물 D controller
+  // 게시물을 지우는 것이기에 따로 return은 하지 않겠음
+  @Delete('/:id')
+  deleteBoard(@Param('id') id: string): void {
+    this.boardsService.deleteBoard(id);
+  }
+  // U 기능 controller
+  @Patch('/:id/status')
+  updateBoardStatus(
+    @Param('id') id: string,
+    @Body('status') status: BoardStatus,
+  ) {
+    return this.boardsService.updateBoardStatus(id, status);
   }
 }
--- src/boards/boards.service.ts
+++ src/boards/boards.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@nestjs/common';
 import { Board, BoardStatus } from './board.model';
 import { v1 as uuid } from 'uuid';
+import { CreateBoardDto } from 'src/dto/create-board.dto';
 
 @Injectable()
 export class BoardsService {
@@ -9,14 +10,31 @@ export class BoardsService {
     return this.boards;
   }
   // CRUD에서의 C
-  createBoard(title: string, description: string) {
+  createBoard(createBoardDto: CreateBoardDto): Board {
+    const { title, description } = createBoardDto;
+
     const board: Board = {
       id: uuid(),
-      title,
-      description,
+      title: title,
+      description: description,
       status: BoardStatus.PUBLIC,
     };
     this.boards.push(board);
+    return board;
+  }
+  // CRUD에서의 특정 게시물 R
+  getBoardById(id: string): Board {
+    return this.boards.find((board) => board.id === id);
+  }
+  // CRUD에서의 특정 게시물 D
+  deleteBoard(id: string): void {
+    // boards에는 filter를 통해 id가 다른 것만 남기겠다.
+    this.boards = this.boards.filter((board) => board.id !== id);
+  }
+  // CRUD에서의 U
+  updateBoardStatus(id: string, status: BoardStatus): Board {
+    const board = this.getBoardById(id);
+    board.status = status;
     return board;
   }
 }
