9442735d5b02fc643d70e2e749852517fb5faac4
shch989 <tankpop11@gmail.com>
2022-05-31 23:47:21 +0900

    Password Hash

--- src/auth/dto/auth-credential.dto.ts
+++ src/auth/dto/auth-credential.dto.ts
@@ -1,4 +1,17 @@
+import { IsString, Matches, MaxLength, MinLength } from 'class-validator';
+
 export class AuthCredentialsDto {
+  @IsString()
+  @MinLength(2)
+  @MaxLength(5)
   username: string;
+
+  @IsString()
+  @MinLength(4)
+  @MaxLength(20)
+  // 영어랑 숫자만 가능한 유효성 체크
+  @Matches(/^[a-zA-Z0-9]*$/, {
+    message: '비밀번호는 오직 영어와 숫자만 사용 가능합니다',
+  })
   password: string;
 }
--- src/auth/user.repository.ts
+++ src/auth/user.repository.ts
@@ -1,12 +1,27 @@
+import {
+  ConflictException,
+  InternalServerErrorException,
+} from '@nestjs/common';
 import { EntityRepository, Repository } from 'typeorm';
 import { AuthCredentialsDto } from './dto/auth-credential.dto';
 import { User } from './user.entity';
+import * as bcrypt from 'bcryptjs';
 
 @EntityRepository(User)
 export class UserRepository extends Repository<User> {
   async createUser(authCredentialsDto: AuthCredentialsDto): Promise<void> {
     const { username, password } = authCredentialsDto;
-    const user = this.create({ username, password });
-    await this.save(user);
+    const salt = await bcrypt.genSalt();
+    const hashedPassword = await bcrypt.hash(password, salt);
+    const user = this.create({ username, password: hashedPassword });
+    try {
+      await this.save(user);
+    } catch (error) {
+      if (error.code === '23505') {
+        throw new ConflictException('해당 이름의 회원이 이미 존재합니다.');
+      } else {
+        throw new InternalServerErrorException();
+      }
+    }
   }
 }
